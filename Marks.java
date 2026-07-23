c3897dbceb1d092dfd0df9b654e7380f8f59deb9
JATNJayalath <it25102766@my.sliit.lk>
2026-07-23 22:59:32 +0530

    Question2

--- Marks.java
+++ Marks.java
@@ -5,26 +5,80 @@ public class Marks{
 
 		Scanner input = new Scanner(System.in);
 
-		System.out.println("Enter number of students: ");
+		System.out.print("Enter number of students: ");
 		int n = input.nextInt();
 
 		int[][] marks = new int [n][3];
 		
+		System.out.println("Subject IDs: 1-Mathematics, 2-Chemistry, 3-Physics");
+
 		for(int i=0; i<n; i++){
 			for(int j=0; j<3; j++){
-				System.out.println("Enter subject"+ (j+1) + "marks of student" +(i+1));
+				System.out.println("Enter subject"+ (j+1) + " " + "marks of student" +(i+1));
 				marks[i][j] = input.nextInt();
 			}
 		}
-		
-		System.out.println("----Menu----");
-		System.out.println("1. Update Marks");
-		System.out.println("2. Average mark of a Subject");
-		System.out.println("3. Average mark of a Student");
-		System.out.println("4. Total mark of a Student");
+		int choice;
 
-		System.out.println("Enter your choice: ");
-		int choice = 
+		do{
+			
+			System.out.println("----Menu----");
+			System.out.println("1. Update Marks");
+			System.out.println("2. Average mark of a Subject");
+			System.out.println("3. Average mark of a Student");
+			System.out.println("4. Total mark of a Student");
+
+			System.out.print("Enter your choice: ");
+			choice = input.nextInt();
+
+			switch(choice){
+				case 1:
+					System.out.println("Enter student ID: ");
+					int studentID = input.nextInt();
+					System.out.println("Enter subject ID: ");
+					int subjectID = input.nextInt();
+				
+					System.out.println("Enter New Mark: ");
+					int newMark = input.nextInt();
+
+					marks[studentID-1][subjectID-1]=newMark;
+
+					System.out.println("Mark updated successfully");
+				
+					break;
+
+				case 2:
+					System.out.println("Enter subject ID: ");
+					subjectID = input.nextInt();
+
+					int sumSubject = 0;
+
+					for(int i=0; i<n; i++){
+						sumSubject += marks[i][subjectID - 1];
+					}
+
+					System.out.println("Average: " + (double)sumSubject / n);
+
+					break;
+				case 3:
+					System.out.println("Enter student ID(1-" + n + "): ");
+					studentID = input.nextInt();
+
+					int sumStudent = 0;
+
+					for(int j=0; j<3; j++){
+						sumStudent += marks[studentID -1][j];
+					}
+
+					System.out.println("Average mark: " + (double)sumStudent / 3);
+
+					break;
+				case 4:
+
+
+			}
+
+		}while(choice != 3);
 
 	}
 }
